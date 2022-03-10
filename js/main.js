78ab3ef12b15bf153f45171e330ee654516f7104
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-03-10 15:53:36 +0300

    header and promo and js
    header and promo and js

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let headerAuth = document.querySelector('.header__auth')
+let headerNav = document.querySelector('.header__nav')
+let headerNavLinks = document.querySelectorAll('.header__nav__link')
+
+function hide () {
+    if (pageYOffset > 89) {
+        headerNav.style.marginTop = '-120px'
+        headerNav.style.background = '#fbf4eb'
+        for (let headerNavLink of headerNavLinks) {
+            headerNavLink.style.color = "#000000"
+            headerNavLink.style.padding = "5px"
+            headerNavLink.style.margin = "15px"
+            headerNav.style.marginLeft = "-750px"
+            headerNav.style.paddingLeft = "1000px"
+            headerNav.style.paddingRight = "1000px"
+        }
+    } else if (pageYOffset < 90) {
+        headerNav.style.marginTop = '0px'
+        headerNav.style.background = 'transparent'
+        headerAuth.style.transition = '0.5s'
+        headerNav.style.transition = '0.5s'
+        for (let headerNavLink of headerNavLinks) {
+            headerNavLink.style.color = "#ffffff"
+            headerNav.style.marginLeft = "250px"
+            headerNav.style.paddingLeft = "0px"
+            headerNav.style.paddingRight = "0px"
+        }
+    }
+}
+
+setInterval(hide)
