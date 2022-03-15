3866d8be9b15e85cef0d97eb68fd9770d2426875
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-03-15 09:43:28 +0300

    btn up
    btn up

--- js/main.js
+++ js/main.js
@@ -29,5 +29,25 @@ function hide () {
         }
     }
 }
-
 setInterval(hide)
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    let toTopBtn = document.querySelector('.up');
+
+    window.onscroll = function () {
+        if (window.pageYOffset > 580) {
+            toTopBtn.style.display = 'flex'
+        } else {
+            toTopBtn.style.display = 'none'
+        }
+    }
+
+    // плавный скролл наверх 
+    toTopBtn.addEventListener('click', function () {
+        window.scrollBy({
+            top: -document.documentElement.scrollHeight,
+            behavior: 'smooth'
+        });
+    });
+});
