cf3e909298917d18a905eab5a505c615e67bc85e
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-03-15 10:47:50 +0300

    add no-funk modal
    add no-funk modal

--- js/main.js
+++ js/main.js
@@ -6,26 +6,26 @@ function hide () {
     if (pageYOffset > 89) {
         headerNav.style.marginTop = '-120px'
         headerNav.style.background = '#fbf4eb'
+        headerNav.style.marginLeft = "-1300px"
+        headerNav.style.paddingLeft = "1000px"
+        headerNav.style.paddingRight = "1000px"
         for (let headerNavLink of headerNavLinks) {
             headerNavLink.style.color = "#000000"
             headerNavLink.transition = '0.8s'
             headerNavLink.style.padding = "5px"
             headerNavLink.style.margin = "15px"
-            headerNav.style.marginLeft = "-700px"
-            headerNav.style.paddingLeft = "1000px"
-            headerNav.style.paddingRight = "1000px"
         }
     } else if (pageYOffset < 90) {
         headerNav.style.marginTop = '0px'
         headerNav.style.background = 'transparent'
         headerAuth.style.transition = '0.5s'
         headerNav.style.transition = '0.5s'
+        headerNav.style.marginLeft = "-300px"
+        headerNav.style.paddingLeft = "0px"
+        headerNav.style.paddingRight = "0px"
         for (let headerNavLink of headerNavLinks) {
             headerNavLink.style.color = "#ffffff"
             headerNavLink.transition = '0.8s'
-            headerNav.style.marginLeft = "300px"
-            headerNav.style.paddingLeft = "0px"
-            headerNav.style.paddingRight = "0px"
         }
     }
 }
