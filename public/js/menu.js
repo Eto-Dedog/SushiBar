be05c9e9af41e120270a4ba968de1a220d69e2d4
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-04-18 09:25:37 +0300

    fix js fix html
    fix js fix html

--- public/js/menu.js
+++ public/js/menu.js
@@ -9,97 +9,13 @@ for (let i = 0; i < btnsMenu.length; i++) {
     });
 }
 
-// btnsMenu[0].classList.add('categories__card-active')
-//
-// btnsMenu[0].addEventListener('click', function (){
-//     btnsMenu[0].classList.add('categories__card-active')
-//     btnsMenu[1].classList.remove('categories__card-active')
-//     btnsMenu[2].classList.remove('categories__card-active')
-//     btnsMenu[3].classList.remove('categories__card-active')
-//     btnsMenu[4].classList.remove('categories__card-active')
-//     btnsMenu[5].classList.remove('categories__card-active')
-//     btnsMenu[6].classList.remove('categories__card-active')
-//     btnsMenu[7].classList.remove('categories__card-active')
-//
-// })
-//
-// btnsMenu[1].addEventListener('click', function (){
-//     btnsMenu[1].classList.add('categories__card-active')
-//     btnsMenu[0].classList.remove('categories__card-active')
-//     btnsMenu[2].classList.remove('categories__card-active')
-//     btnsMenu[3].classList.remove('categories__card-active')
-//     btnsMenu[4].classList.remove('categories__card-active')
-//     btnsMenu[5].classList.remove('categories__card-active')
-//     btnsMenu[6].classList.remove('categories__card-active')
-//     btnsMenu[7].classList.remove('categories__card-active')
-// })
-//
-// btnsMenu[2].addEventListener('click', function (){
-//     btnsMenu[2].classList.add('categories__card-active')
-//     btnsMenu[0].classList.remove('categories__card-active')
-//     btnsMenu[1].classList.remove('categories__card-active')
-//     btnsMenu[3].classList.remove('categories__card-active')
-//     btnsMenu[4].classList.remove('categories__card-active')
-//     btnsMenu[5].classList.remove('categories__card-active')
-//     btnsMenu[6].classList.remove('categories__card-active')
-//     btnsMenu[7].classList.remove('categories__card-active')
-// })
-//
-// btnsMenu[3].addEventListener('click', function (){
-//     btnsMenu[3].classList.add('categories__card-active')
-//     btnsMenu[0].classList.remove('categories__card-active')
-//     btnsMenu[1].classList.remove('categories__card-active')
-//     btnsMenu[2].classList.remove('categories__card-active')
-//     btnsMenu[4].classList.remove('categories__card-active')
-//     btnsMenu[5].classList.remove('categories__card-active')
-//     btnsMenu[6].classList.remove('categories__card-active')
-//     btnsMenu[7].classList.remove('categories__card-active')
-// })
-//
-// btnsMenu[4].addEventListener('click', function (){
-//     btnsMenu[4].classList.add('categories__card-active')
-//     btnsMenu[0].classList.remove('categories__card-active')
-//     btnsMenu[1].classList.remove('categories__card-active')
-//     btnsMenu[2].classList.remove('categories__card-active')
-//     btnsMenu[3].classList.remove('categories__card-active')
-//     btnsMenu[5].classList.remove('categories__card-active')
-//     btnsMenu[6].classList.remove('categories__card-active')
-//     btnsMenu[7].classList.remove('categories__card-active')
-// })
-//
-// btnsMenu[5].addEventListener('click', function (){
-//     btnsMenu[5].classList.add('categories__card-active')
-//     btnsMenu[0].classList.remove('categories__card-active')
-//     btnsMenu[1].classList.remove('categories__card-active')
-//     btnsMenu[2].classList.remove('categories__card-active')
-//     btnsMenu[3].classList.remove('categories__card-active')
-//     btnsMenu[4].classList.remove('categories__card-active')
-//     btnsMenu[6].classList.remove('categories__card-active')
-//     btnsMenu[7].classList.remove('categories__card-active')
-// })
-//
-// btnsMenu[6].addEventListener('click', function (){
-//     btnsMenu[6].classList.add('categories__card-active')
-//     btnsMenu[0].classList.remove('categories__card-active')
-//     btnsMenu[1].classList.remove('categories__card-active')
-//     btnsMenu[2].classList.remove('categories__card-active')
-//     btnsMenu[3].classList.remove('categories__card-active')
-//     btnsMenu[4].classList.remove('categories__card-active')
-//     btnsMenu[5].classList.remove('categories__card-active')
-//     btnsMenu[7].classList.remove('categories__card-active')
-// })
-//
-// btnsMenu[7].addEventListener('click', function (){
-//     btnsMenu[7].classList.add('categories__card-active')
-//     btnsMenu[0].classList.remove('categories__card-active')
-//     btnsMenu[1].classList.remove('categories__card-active')
-//     btnsMenu[2].classList.remove('categories__card-active')
-//     btnsMenu[3].classList.remove('categories__card-active')
-//     btnsMenu[4].classList.remove('categories__card-active')
-//     btnsMenu[5].classList.remove('categories__card-active')
-//     btnsMenu[6].classList.remove('categories__card-active')
-// })
+btnsMenu[0].classList.add('categories__card-active')
 
-
-
-
+for (let i = 0; i < btnsMenu.length; i++) {
+    btnsMenu[i].addEventListener('click', function() {
+        for (let i = 0; i < btnsMenu.length; i++) {
+            btnsMenu[i].classList.remove('categories__card-active');
+        }
+        btnsMenu[i].classList.add('categories__card-active');
+    });
+}
--- public/js/products.js
+++ public/js/products.js
@@ -10,102 +10,13 @@ for (let i = 0; i < sortsBtns.length; i++) {
     });
 }
 
-// for (let i = 0; i < sortsBtns.length; i++) {
-//     sortsBtns[i].addEventListener('click', function() {
-//         for (let i = 0; i < sortsBox.length; i++) {
-//             sortsBtns[i].classList.remove('.sorts__category-btn-active');
-//         }
-//         sortsBtns[i].classList.add('.sorts__category-btn-active');
-//     });
-// }
+sortsBtns[0].classList.add('sorts__category-btn-active')
 
-// sortsBtns[0].classList.add('sorts__category-btn-active')
-//
-// sortsBtns[0].addEventListener('click', function (){
-//     sortsBtns[0].classList.add('sorts__category-btn-active')
-//     sortsBtns[1].classList.remove('sorts__category-btn-active')
-//     sortsBtns[2].classList.remove('sorts__category-btn-active')
-//     sortsBtns[3].classList.remove('sorts__category-btn-active')
-//     sortsBtns[4].classList.remove('sorts__category-btn-active')
-//     sortsBtns[5].classList.remove('sorts__category-btn-active')
-//     sortsBtns[6].classList.remove('sorts__category-btn-active')
-//     sortsBtns[7].classList.remove('sorts__category-btn-active')
-//
-// })
-//
-// sortsBtns[1].addEventListener('click', function (){
-//     sortsBtns[1].classList.add('sorts__category-btn-active')
-//     sortsBtns[0].classList.remove('sorts__category-btn-active')
-//     sortsBtns[2].classList.remove('sorts__category-btn-active')
-//     sortsBtns[3].classList.remove('sorts__category-btn-active')
-//     sortsBtns[4].classList.remove('sorts__category-btn-active')
-//     sortsBtns[5].classList.remove('sorts__category-btn-active')
-//     sortsBtns[6].classList.remove('sorts__category-btn-active')
-//     sortsBtns[7].classList.remove('sorts__category-btn-active')
-// })
-//
-// sortsBtns[2].addEventListener('click', function (){
-//     sortsBtns[2].classList.add('sorts__category-btn-active')
-//     sortsBtns[0].classList.remove('sorts__category-btn-active')
-//     sortsBtns[1].classList.remove('sorts__category-btn-active')
-//     sortsBtns[3].classList.remove('sorts__category-btn-active')
-//     sortsBtns[4].classList.remove('sorts__category-btn-active')
-//     sortsBtns[5].classList.remove('sorts__category-btn-active')
-//     sortsBtns[6].classList.remove('sorts__category-btn-active')
-//     sortsBtns[7].classList.remove('sorts__category-btn-active')
-// })
-
-// sortsBtns[3].addEventListener('click', function (){
-//     sortsBtns[3].classList.add('sorts__category-btn-active')
-//     sortsBtns[0].classList.remove('sorts__category-btn-active')
-//     sortsBtns[1].classList.remove('sorts__category-btn-active')
-//     sortsBtns[2].classList.remove('sorts__category-btn-active')
-//     sortsBtns[4].classList.remove('sorts__category-btn-active')
-//     sortsBtns[5].classList.remove('sorts__category-btn-active')
-//     sortsBtns[6].classList.remove('sorts__category-btn-active')
-//     sortsBtns[7].classList.remove('sorts__category-btn-active')
-// })
-//
-// sortsBtns[4].addEventListener('click', function (){
-//     sortsBtns[4].classList.add('sorts__category-btn-active')
-//     sortsBtns[0].classList.remove('sorts__category-btn-active')
-//     sortsBtns[1].classList.remove('sorts__category-btn-active')
-//     sortsBtns[2].classList.remove('sorts__category-btn-active')
-//     sortsBtns[3].classList.remove('sorts__category-btn-active')
-//     sortsBtns[5].classList.remove('sorts__category-btn-active')
-//     sortsBtns[6].classList.remove('sorts__category-btn-active')
-//     sortsBtns[7].classList.remove('sorts__category-btn-active')
-// })
-//
-// sortsBtns[5].addEventListener('click', function (){
-//     sortsBtns[5].classList.add('sorts__category-btn-active')
-//     sortsBtns[0].classList.remove('sorts__category-btn-active')
-//     sortsBtns[1].classList.remove('sorts__category-btn-active')
-//     sortsBtns[2].classList.remove('sorts__category-btn-active')
-//     sortsBtns[3].classList.remove('sorts__category-btn-active')
-//     sortsBtns[4].classList.remove('sorts__category-btn-active')
-//     sortsBtns[6].classList.remove('sorts__category-btn-active')
-//     sortsBtns[7].classList.remove('sorts__category-btn-active')
-// })
-//
-// sortsBtns[6].addEventListener('click', function (){
-//     sortsBtns[6].classList.add('sorts__category-btn-active')
-//     sortsBtns[0].classList.remove('sorts__category-btn-active')
-//     sortsBtns[1].classList.remove('sorts__category-btn-active')
-//     sortsBtns[2].classList.remove('sorts__category-btn-active')
-//     sortsBtns[3].classList.remove('sorts__category-btn-active')
-//     sortsBtns[4].classList.remove('sorts__category-btn-active')
-//     sortsBtns[5].classList.remove('sorts__category-btn-active')
-//     sortsBtns[7].classList.remove('sorts__category-btn-active')
-// })
-//
-// sortsBtns[7].addEventListener('click', function (){
-//     sortsBtns[7].classList.add('sorts__category-btn-active')
-//     sortsBtns[0].classList.remove('sorts__category-btn-active')
-//     sortsBtns[1].classList.remove('sorts__category-btn-active')
-//     sortsBtns[2].classList.remove('sorts__category-btn-active')
-//     sortsBtns[3].classList.remove('sorts__category-btn-active')
-//     sortsBtns[4].classList.remove('sorts__category-btn-active')
-//     sortsBtns[5].classList.remove('sorts__category-btn-active')
-//     sortsBtns[6].classList.remove('sorts__category-btn-active')
-// })
+for (let i = 0; i < sortsBtns.length; i++) {
+    sortsBtns[i].addEventListener('click', function() {
+        for (let i = 0; i < sortsBtns.length; i++) {
+            sortsBtns[i].classList.remove('sorts__category-btn-active');
+        }
+        sortsBtns[i].classList.add('sorts__category-btn-active');
+    });
+}
