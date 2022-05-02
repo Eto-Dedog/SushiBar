61146f02bbc01ce8992eb538320d3b57d413bb1e
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-05-02 21:45:11 +0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'IndexController@index')->name('index.index');
 
-//Route::get('/', function () {
-//    return view('index');
-//});
-
 Route::get('/about', 'AboutController@index')->name('about.index');
-
-
-//Route::get('/about', function () {
-//    return view('about');
-//});
 
 Route::post('/mailing', 'MailingController@store')->name('mailing.store');
 Route::get('/mailer', 'MailingController@index')->name('mailing.index');
@@ -35,23 +26,13 @@ Route::DELETE('/feedback/{id}', 'FeedbackController@destroy')->name('feedback.de
 
 Route::get('/account', 'AccountController@index')->name('account.index');
 
-//Route::get('/account', function () {
-//    return view('account');
-//});
+
 Route::get('/all-reviews', 'AllCommentsController@index')->name('allComments.index');
 Route::get('/all-comments', 'AllReviewsController@index')->name('allReviews.index');
-
-//Route::get('/all-comments', function () {
-//    return view('all-comments');
-//});
 
 Route::get('/c-e-categories', 'ReviewsController@create')->name('reviews.create');
 Route::post('/post', 'ReviewsController@store')->name('reviews.store');
 Route::DELETE('/blog/post/{id}', 'ReviewsController@destroy')->name('reviews.destroy');
-
-//Route::get('/all-reviews', function () {
-//    return view('all-reviews');
-//});
 
 Route::get('/blog', 'PostsController@index')->name('posts.index');
 Route::get('/c-post', 'PostsController@create')->name('posts.create');
@@ -61,45 +42,14 @@ Route::get('/blog/post/edit/{id}', 'PostsController@edit')->name('posts.edit');
 Route::PATCH('/blog/post/{id}', 'PostsController@update')->name('posts.update');
 Route::DELETE('/blog/post/del/{id}', 'PostsController@destroy')->name('posts.destroy');
 
-// Route::get('/blog', function () {
-//     return view('blog');
-// });
-
-//Route::get('/c-e-categories', function () {
-//    return view('c-e-categories');
-//});
-//
-//Route::get('/c-e-post', function () {
-//    return view('c-e-post');
-//});
-//
-//Route::get('/c-e-product', function () {
-//    return view('c-e-product');
-//});
-
 Route::get('/contacts', 'ContactsController@index')->name('contacts.index');
-
-//Route::get('/contacts', function () {
-//    return view('contacts');
-//});
 
 Route::get('/information', 'InformationController@index')->name('info.index');
 
-//Route::get('/information', function () {
-//    return view('information');
-//});
-
 Route::get('/menu', 'ProductsController@index')->name('product.index');
-
-//Route::get('/menu', function () {
-//    return view('menu');
-//});
 
 Route::get('/post', 'PostController@index')->name('post.index');
 
-//Route::get('/post', function () {
-//    return view('post');
-//});
 
 Route::get('/product', 'CategoriesItemController@index')->name('categoriesItem.index');
 Route::get('/c-product'   , 'CategoriesItemController@create')->name('categoriesItem.create');
@@ -108,10 +58,6 @@ Route::get('/products/product/{id}', 'CategoriesItemController@show')->name('cat
 Route::get('/products/product/edit/{id}', 'CategoriesItemController@edit')->name('categoriesItem.edit');
 Route::PATCH('/products/product/{id}', 'CategoriesItemController@update')->name('categoriesItem.update');
 Route::DELETE('/products/product/del/{id}', 'CategoriesItemController@destroy')->name('categoriesItem.destroy');
-
-//Route::get('/product', function () {
-//    return view('product');
-//});
 
 Route::get('/products', 'CategoriesController@index')->name('categories.index');
 Route::get('/c-categories', 'CategoriesController@create')->name('categories.create');
@@ -127,10 +73,6 @@ Route::DELETE('/products/product/{id}', 'CommentsController@destroy')->name('com
 
 Route::PATCH('/account/{id}', 'UserInfoController@update')->name('users.update');
 
-
-//Route::get('/products', function () {
-//    return view('products');
-//});
 
 Auth::routes();
 
